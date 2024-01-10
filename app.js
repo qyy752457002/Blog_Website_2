b10fa3f80d6360b68679f73d1aff34096962de35
Yiyu Qian <yiyuqianunitediting@gmail.com>
2024-01-10 15:48:41 +0800

    update get

--- app.js
+++ app.js
@@ -51,22 +51,6 @@ const blogsSchema = new mongoose.Schema({
 // create model from schema as a collection
 const Blog = mongoose.model('Blog', blogsSchema)
 
-//default posts
-const firstPost = new Blog({
-	blogTitle: 'Cupcakes',
-	blogBody:
-		'Cupcake ipsum dolor sit amet candy canes apple pie oat cake gummies. I love dessert muffin oat cake pudding I love. Cotton candy wafer cookie candy canes lemon drops pie marshmallow gingerbread. Sesame snaps chupa chups bonbon I love danish bear claw. Bonbon powder jelly beans jelly gummi bears jelly beans. Chocolate cake jujubes I love dragée jelly shortbread liquorice. Gummies I love jelly-o fruitcake lollipop. Sesame snaps muffin chocolate bar chocolate bar sugar plum soufflé. Tart tootsie roll topping pie lollipop powder topping icing. I love soufflé brownie chocolate jelly-o dessert apple pie cookie. Halvah cake chocolate jelly sweet sugar plum bonbon donut. Croissant candy sweet roll gingerbread tart cake. Marshmallow jelly chupa chups donut I love. Pastry cotton candy tiramisu muffin I love bonbon sweet tiramisu.',
-	blogCreated: day,
-})
-const secondPost = new Blog({
-	blogTitle: 'Muffins',
-	blogBody:
-		'I love apple pie dragée powder cupcake donut lollipop. Croissant gummi bears chocolate bar I love I love cheesecake lollipop tart. I love tootsie roll toffee marzipan lemon drops cake muffin. I love lollipop powder croissant tiramisu. Sweet roll cheesecake jelly beans sweet gummi bears ice cream marzipan. Biscuit jelly beans I love I love tart gummies macaroon chocolate cake. Toffee candy canes toffee I love cupcake gingerbread carrot cake gummi bears halvah. Dragée oat cake biscuit fruitcake gummi bears pudding marshmallow I love. Powder wafer marshmallow dragée liquorice powder pudding bonbon lollipop. Carrot cake candy I love cookie candy. Candy canes I love sesame snaps muffin candy canes apple pie I love chocolate bar. Dessert marzipan bonbon carrot cake chocolate cake apple pie danish jelly beans. Cookie I love cake sweet roll toffee I love caramels carrot cake croissant. Pastry gummies cake cookie chupa chups. Carrot cake tootsie roll cheesecake jelly wafer halvah bonbon icing pudding. Lollipop gingerbread fruitcake ice cream icing powder. Pie oat cake caramels marzipan halvah gummi bears apple pie lollipop gingerbread. Donut I love powder chocolate bar I love chocolate sweet roll. Muffin sesame snaps I love chupa chups cake bonbon. Gingerbread soufflé donut I love lemon drops I love lemon drops. I love gummi bears muffin lollipop gingerbread icing carrot cake. I love I love lollipop fruitcake cotton candy macaroon bonbon donut. Gingerbread gummi bears tart sugar plum cookie jelly-o. I love halvah pudding I love biscuit jelly beans jujubes sweet roll. Chocolate bar sweet bear claw I love caramels bonbon icing. Danish I love brownie wafer donut oat cake topping chocolate lemon drops.',
-	blogCreated: day,
-})
-
-const blogsArr = []
-
 // Add this route handler before your other routes
 app.get('/favicon.ico', (req, res) => {
 	// Respond with a 204 No Content status code
@@ -75,35 +59,22 @@ app.get('/favicon.ico', (req, res) => {
 
 //get requests
 app.get('/', (req, res) => {
-	if (res.statusCode === 200) {
-		//get all tasks from DB collection and put it on home page
-		Blog.find({})
-			.then(blogs => {
-				if (blogs.length === 0) {
-					// Function call
-					Blog.insertMany(blogsArr)
-						.then(function () {
-							console.log('Data inserted') // Success
-							res.redirect('/')
-						})
-						.catch(function (error) {
-							console.log(error) // Failure
-						})
-				} else {
-					res.render('home', {
-						startingContent: homeContent,
-						blogs: blogs,
-						mainTitle: 'Home',
-					})
-				}
-			})
-			.catch(function (err) {
-				console.log(err)
-			})
-	} else {
-		console.log('Not getting DB blogs')
-	}
-})
+    Blog.find({}) // Fetch all blogs from the database
+        .then(blogs => {
+            // Render home page with blogs
+            res.render('home', {
+                startingContent: homeContent,
+                blogs: blogs,
+                mainTitle: 'Home',
+            });
+        })
+        .catch(err => {
+            // Handle any errors during database fetch
+            console.error(err);
+            res.status(500).send('Error occurred while fetching data');
+        });
+});
+
 
 app.get('/about', (req, res) => {
 	res.render('about', { content: aboutContent, mainTitle: 'About' })
